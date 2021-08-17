c341d2afe1116891187386367ec539e9633878f6
johnnybueno <johnny.a.bueno1991@gmail.com>
2021-08-17 00:30:21 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#buscar palavras no vetor
+
+estados = [
+    "Acre",
+    "Alagoas",
+    "Amapá",
+    "Amazonas",
+    "Bahia",
+    "Ceará",
+    "Distrito Federal",
+    "Espírito Santo",
+    "Goiás",
+    "Maranhão",
+    "Mato Grosso",
+    "Mato Grosso do Sul",
+    "Minas Gerais",
+    "Pará",
+    "Paraíba",
+    "Paraná",
+    "Pernambuco",
+    "Piauí",
+    "Rio de Janeiro",
+    "Rio Grande do Norte",
+    "Rio Grande do Sul",
+    "Rondônia",
+    "Roraima",
+    "Santa Catarina",
+    "São Paulo",
+    "Sergipe",
+    "Tocantins"
+]
+
+i = 0
+while 1 < 27:
+    if 'Rio' in estados[i] or 'Par' in estados[i]:
+        print(estados[i])
+    i += 1
